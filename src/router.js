066a0b7748904e84b728392866e6bef73fe057ab
matheusabreu <matheusabreu.dev@gmail.com>
2018-09-12 16:40:23 -0300

    chore(router): handle not found router and display a view

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Products from './views/Products.vue'
+import NotFound from './views/NotFound.vue'
 
 Vue.use(Router)
 
@@ -26,6 +27,10 @@ export default new Router({
       path: '/products',
       name: 'products',
       component: Products
+    },
+    {
+      path: '/**', 
+      component: NotFound
     }
   ]
 })
---
+++ src/views/NotFound.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h3>Oh Oh... Page not found!!!</h3>
+    <footer>
+      <small>
+        Made by Matheus Abreu
+      </small>
+    </footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  small {
+    color: #2196f3
+  }
+}
+</style>
